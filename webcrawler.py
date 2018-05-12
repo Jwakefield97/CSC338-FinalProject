fbc56aceb79e01e92bb016aef68ebf1dd0149c69
jwakefield97 <jacobstevenwakefield@gmail.com>
2018-05-11 21:16:00 -0500

    update doc strings

--- webcrawler.py
+++ webcrawler.py
@@ -3,24 +3,15 @@
     Authors: Connor Jansen (cjjansen95), Eric McCullough (eam96), Jacob Wakefield (jwakefield97), John Bell (jab331)
     Assignment: CSC338 final project
 
-    Description: A web crawler that takes an inital url from the command line and collects the links on that page. It then
-                 links that are from the same origin. From those links it visits links that are from the same origin. It continues
-                 this process until no more links from the starting origin are found. In other words it crawls through a website discovering
-                 all of it's pages. The program does this by detecting how many cpu's the computer running it has, it then splits the initial
-                 load (links) from the entry page amongst the processes which spawn threads to handle any further links that are discovered on
-                 subsequent pages.
+    Description: A web crawler that takes an inital url from the command line and collects the links on that page. It then breaks those links up amongst 
+                 the number of cpu's in the computer. After the processes join, if the cycles that the users selected via command line argument, the 
+                 process of spliting the links up and spawning processes starts again on the freshly found links. Once the cycles are reached, the links
+                 that were visited are placed into a text file (output.txt) and the total execuetion time is printed. 
     Tech: Python 3, multiprocessing, threading, time (timing of threads/processes and overall program), sys (command line args),
           beautiful soup (parse html response), and requests (make network requests)
-
-    Execution Flow: command line arg (entry domain) is passed to parsePage() which collects the all the links that are on the same domain.
-                    The processManager() function is then called which sets up the processes based on the system cpu count and spreads
-                    the links collected from  the initial page across the processes. Once the processes are initalized, they are all started.
-                    Once the process is started it initializes a pool of threads to be used in the recursive link collection process.
-                    Once a link is found on the same origin, a thread (one that is not busy working on another page) is used to process
-                    the page and collect links (thread calls parsePage() which visits the page and processes links).
-                    If all threads in the pool are busy then the process waits until a thread is freed up. Once each process is done collecting
-                    /visiting the links it calls output() which waits for all processes to complete before gathering stats (execuetion time, number
-                    of links etc.) and outputing all the links to a file.
+    Libraries to install: beautifulsoup4 and requests. All other libraries used come with python3.
+    How to use: py https://domaintovisit.com <number of cycles>
+                cycles are how many times you want the crawler to break up the links found and spawn processes to visit those links. 
 """
 
 from bs4 import BeautifulSoup
